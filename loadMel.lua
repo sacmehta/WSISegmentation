2265a425f977726c6fd7c10d7d4730ae84c9c098
Sachin Mehta <sacmehta@uw.edu>
2017-09-29 17:17:04 -0700

    removed mean and std statements

--- loadMel.lua
+++ loadMel.lua
@@ -36,37 +36,6 @@ trainIm1FileList = {}
 trainLblFileList = {}
 labelAddVal = 0
 
--- compute the mean and standard deviation for training data
--- do it offline
---local mean1 = {0.72075905156214, 0.66621883843212, 0.74148164596736}
---local std1 = {0.27756112640812, 0.28396234426101, 0.26049176971491}
---
-local mean1 = {
-  0.76453436636411,
-  0.70927286806379,
-  0.77908841987267
-}
-local std1 = {
-  0.17024265830468,
-  0.19564359777311,
-  0.15810968250769
-}
-
---local mean2 = {0.4572846486672, 0.43745873801202, 0.40406985094371}
---local std2 = {0.26742718836252, 0.26436273357389, 0.27838534104728}
-
---local mean2 = {0.16032004955071, 0.15418645213278}
---local std2 = {0.13351542658835, 0.12055973806927}
-
-local mean2 = {
-  0.15197299610137,
-  0.15640383657466
-}
-local std2 = {
-  0.11836776727387,
-  0.13015500535413
-}
-
 
 
 local histClasses = torch.Tensor(classes):fill(0)
@@ -88,12 +57,6 @@ else
     labelIm = image.scale(labelIm, opt.imWidth, opt.imHeight, 'simple')
     labelIm:add(labelAddVal)
     labelIm[labelIm:eq(0)] = 1
---    labelIm[labelIm:eq(3)] = 2
---    labelIm[labelIm:eq(4)] = 3
---    labelIm[labelIm:eq(5)] = 3
---    labelIm[labelIm:eq(7)] = 3
---    labelIm[labelIm:eq(6)] = 4
---    labelIm[labelIm:eq(8)] = 5
     histClasses = histClasses + torch.histc(labelIm:float(), classes, 1, classes)
     assert(torch.max(labelIm) <= classes and torch.min(labelIm) > 0, 'Label values should be between 1 and number of classes: max ' .. torch.max(labelIm) .. ' min: ' .. torch.min(labelIm))
     
@@ -121,9 +84,10 @@ else
     
     local labelIm = image.load(valLblFileList[lineNo], 1, 'byte')
     --scale the label image using simple interpolation
-    --labelIm = image.scale(labelIm, opt.imWidth, opt.imHeight, 'simple')
-    --labelIm:add(labelAddVal)
-    --assert(torch.max(labelIm) <= classes and torch.min(labelIm) > 0, 'Label values should be between 1 and number of classes: max ' .. torch.max(labelIm) .. ' min: ' .. torch.min(labelIm))
+    labelIm = image.scale(labelIm, opt.imWidth, opt.imHeight, 'simple')
+    labelIm:add(labelAddVal)
+    labelIm[labelIm:eq(0)] = 1
+    assert(torch.max(labelIm) <= classes and torch.min(labelIm) > 0, 'Label values should be between 1 and number of classes: max ' .. torch.max(labelIm) .. ' min: ' .. torch.min(labelIm))
     
     lineNo = lineNo + 1
   end
@@ -155,10 +119,6 @@ dataCache.trainlbl = trainLblFileList
 dataCache.valIm = valImFileList
 dataCache.valIm1 = valIm1FileList
 dataCache.vallbl = valLblFileList
-dataCache.mean1 = mean1
-dataCache.std1 = std1
-dataCache.mean2 = mean2
-dataCache.std2 = std2
 dataCache.classes = classes
 dataCache.labelAddVal = labelAddVal
 dataCache.classWeight = classWeights
